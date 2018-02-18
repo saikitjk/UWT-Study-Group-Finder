96671750fd9fe86eb9aa2cf60d08727c63f18fde
KWONG <saikitjk@insttech.washington.edu>
2018-02-17 22:38:18 -0800

    Project

---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string GroupName { get; set; }
+        public string FullName { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+
+    }
+}
